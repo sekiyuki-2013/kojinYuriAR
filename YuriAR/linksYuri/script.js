94f87d6e26bb0340206b57117bddf35e5c4c8b74
sekiyuki-2013 <147236095+sekiyuki-2013@users.noreply.github.com>
2025-12-23 03:03:32 +0900

    Add files via upload

---
+++ YuriAR/linksYuri/script.js
@@ -0,0 +1,44 @@
+const video = document.getElementById("video");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const chara = new Image();
+chara.src = "linksYuri.PNG"; // 2Dキャラ画像
+
+// 外カメラ起動
+navigator.mediaDevices.getUserMedia({
+  video: {
+    facingMode: { ideal: "environment" }
+  },
+  audio: false
+}).then(stream => {
+  video.srcObject = stream;
+});
+
+// 描画ループ
+function draw() {
+  if (video.videoWidth === 0) {
+    requestAnimationFrame(draw);
+    return;
+  }
+
+  canvas.width = video.videoWidth;
+  canvas.height = video.videoHeight;
+
+  // カメラ
+  ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+  // キャラ（右側に立たせる例）
+  const size = canvas.width * 0.45;
+  ctx.drawImage(
+    chara,
+    canvas.width - size - 200,
+    canvas.height - size  + 10,
+    size,
+    size
+  );
+
+  requestAnimationFrame(draw);
+}
+
+draw();
