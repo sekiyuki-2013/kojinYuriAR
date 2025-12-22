4e7e40f35aa55458f846463947733225b170b07d
sekiyuki-2013 <147236095+sekiyuki-2013@users.noreply.github.com>
2025-12-23 03:18:11 +0900

    Update script.js

--- YuriAR/originalYuri/script.js
+++ YuriAR/originalYuri/script.js
@@ -29,11 +29,11 @@ function draw() {
   ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
 
   // キャラ（右側に立たせる例）
-  const size = canvas.width * 0.35;
+  const size = canvas.width * 1.1;
   ctx.drawImage(
     chara,
     canvas.width - size - 210,
-    canvas.height - size,
+    canvas.height - size + 100,
     size,
     size
   );
